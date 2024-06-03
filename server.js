a66be49c8c0697989e9ffad76d46110d8b4f9ef4
vmaksimov <viktor.maksimov@oblak.bg>
2024-06-03 15:52:27 +0300

    adding web sockets pong game

---
+++ server.js
@@ -0,0 +1,28 @@
+
+const server = require('http').createServer();
+
+const io = require('socket.io')(server , {
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST']
+    }
+  });
+
+const PORT = 3000;
+
+server.listen(PORT);
+console.log(`Listening on port ${PORT}...`)
+
+let readyPlayerCoujnt = 0;
+
+io.on('connection', (socket) => {
+    console.log('a user connected', socket.id);
+
+    socket.on('ready', () => {
+        console.log('Player ready', socket.id)
+        readyPlayerCoujnt++
+        if (readyPlayerCoujnt === 2){
+            io.emit('startGame', socket.id)
+        }
+    })
+})
